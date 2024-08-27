a7c015c6052c046963420127b5904bf23ffe1dad
Nikodem Czarlinski <nczarlinski@gmail.com>
2024-08-27 19:30:30 +0100

    slow path results

--- design_patterns/slowpath_remove/mybenchmark.cc
+++ design_patterns/slowpath_remove/mybenchmark.cc
@@ -1,54 +1,66 @@
-
 #include <benchmark/benchmark.h>
-#include <emmintrin.h> // SSE2
+#include <map>
+#include <string>
+#include <iostream>
+using namespace std;
 
-const int size = 10000;
+static std::map<int, int> orders;
+static bool okay = false;
 
-void add_arrays(float* a, float* b, float* c, int size) {
-	for (int i = 0; i < size; ++i) {
-    	c[i] = a[i] + b[i];
-	}
+static void sendToClient(const std::string& message) {
+  // Simulate some time consuming operation.
+  for (volatile int i = 0; i < 100000; ++i);
 }
 
-void add_arrays_simd(float* a, float* b, float* c, int size) {
-	__m128 a_chunk, b_chunk, c_chunk;
-
-	for (int i = 0; i < size; i += 4) {
-    	a_chunk = _mm_loadu_ps(&a[i]);
-    	b_chunk = _mm_loadu_ps(&b[i]);
-    	c_chunk = _mm_add_ps(a_chunk, b_chunk);
-    	_mm_storeu_ps(&c[i], c_chunk);
-	}
+static void removeFromMap(int orderId, std::map<int, int>& orders) {
+  orders.erase(orderId);
 }
 
-static void BM_ArrayAddition(benchmark::State& state) {
-	float* a = new float[size];
-	float* b = new float[size];
-	float* c = new float[size];
-
-	for (auto _ : state) {
-    	add_arrays(a, b, c, size);
-	}
-
-	delete[] a;
-	delete[] b;
-	delete[] c;
+static void logMessage(const std::string& message) {
+  // Simulate some time consuming operation.
+  for (volatile int i = 0; i < 100000; ++i);
 }
-BENCHMARK(BM_ArrayAddition);
 
-static void BM_ArrayAddition_SIMD(benchmark::State& state) {
-	float* a = new float[size];
-	float* b = new float[size];
-	float* c = new float[size];
-
-	for (auto _ : state) {
-    	add_arrays_simd(a, b, c, size);
-	}
-
-	delete[] a;
-	delete[] b;
-	delete[] c;
+static void __attribute__ ((noinline)) HandleError() {
+  sendToClient("Order failed");
+  removeFromMap(1, orders);
+  logMessage("Order failed");
 }
-BENCHMARK(BM_ArrayAddition_SIMD);
 
+// Bad scenario.
+static void SlowpathNotRemoved(benchmark::State& state) {
+  int counter = 0;
+  for (auto _ : state) {
+    okay = (++counter % 10 != 0); // Hotpath 90% of the time, slowpath 10% of the time.
+    if (okay) {
+      // Hotpath.
+    } else {
+      // Slow-path
+      sendToClient("Order failed");
+      removeFromMap(1, orders);
+      logMessage("Order failed");
+    }
+  }
+}
+
+// Register the function as a benchmark.
+BENCHMARK(SlowpathNotRemoved);
+
+// Better scenario.
+static void SlowpathRemoved(benchmark::State& state) {
+  int counter = 0;
+  for (auto _ : state) {
+    okay = (++counter % 10 != 0); // Hotpath 90% of the time, slowpath 10% of the time.
+    if (okay) {
+      // Hotpath.
+    } else {
+      HandleError();
+    }
+  }
+}
+
+// Register the function as a benchmark.
+BENCHMARK(SlowpathRemoved);
+
+// Run the benchmark.
 BENCHMARK_MAIN();
